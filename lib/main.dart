b98e501bda2dcdbc18b561c2e61589c1ea91684d
ymdarake <t.sakamoto.ouen@gmail.com>
2019-12-22 16:06:44 +1300

    step4: refactored with AnimatedBuilder to separate responsibilities: Render the logo, Define the Animation object, Render the transition

--- lib/main.dart
+++ lib/main.dart
@@ -44,13 +44,32 @@ class AnimatedLogo extends AnimatedWidget {
 
   Widget build(BuildContext context) {
     final animation = listenable as Animation<double>;
-    return Center(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: FlutterLogo(),
-      ),
-    );
+    return GrowTransition(child: LogoWidget(), animation: animation);
   }
+}
+
+class GrowTransition extends StatelessWidget {
+  GrowTransition({this.child, this.animation});
+
+  final Widget child;
+  final Animation<double> animation;
+
+  Widget build(BuildContext context) => Center(
+    child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) => Container(
+          height: animation.value,
+          width: animation.value,
+          child: child,
+        ),
+        child: child),
+  );
+}
+
+class LogoWidget extends StatelessWidget {
+  // Leave out the height and width so it fills the animating parent
+  Widget build(BuildContext context) => Container(
+    margin: EdgeInsets.symmetric(vertical: 10),
+    child: FlutterLogo(),
+  );
 }
